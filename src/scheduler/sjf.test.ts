04129ba2ee566f22c94ca1ca928f88da53170df8
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-02 18:38:48 -0300

    test: ensure sjf algorithm always choose the process with the fewest remaining instructions

---
+++ src/scheduler/sjf.test.ts
@@ -0,0 +1,18 @@
+import { Process } from "../entities/process.js";
+import { SJFScheduler } from "./sjf.js";
+
+describe("Algoritmo Shortest Job First", () => {
+  test("Deve escolher sempre o processo com menos instruções restantes", () => {
+    const sjf = new SJFScheduler();
+    const processLong = new Process(1, 5);
+    const processShort = new Process(2, 2);
+
+    sjf.add(processLong);
+    sjf.add(processShort);
+
+    expect(sjf.execute()).toMatch(/Processo 2 executando/);
+    expect(sjf.execute()).toMatch(/finalizado e removido/);
+
+    expect(sjf.execute()).toMatch(/Processo 1 executando/);
+  });
+});
