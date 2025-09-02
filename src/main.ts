da29e3403311c217b098b9744eaf8ff6b55d1002
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-02 18:13:41 -0300

    feat: add implementation of rr, fcfs and sjf scheduling functions

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+import { CPU } from "./cpu/cpu.js";
+import { ProcessGenerator } from "./entities/process-generator.js";
+import { FCFSScheduler } from "./scheduler/fcfs.js";
+import { RoundRobinScheduler } from "./scheduler/round-robin.js";
+import { SJFScheduler } from "./scheduler/sjf.js";
+
+async function runRR() {
+  const generator = new ProcessGenerator();
+  const rr = new RoundRobinScheduler(3);
+
+  rr.add(generator.generate());
+  rr.add(generator.generate());
+  rr.add(generator.generate());
+
+  const cpu = new CPU(rr, 5, 300);
+  console.log("=== Executando Round Robin ===");
+  await cpu.run();
+}
+
+async function runFCFS() {
+  const generator = new ProcessGenerator();
+  const fcfs = new FCFSScheduler();
+
+  fcfs.add(generator.generate());
+  fcfs.add(generator.generate());
+  fcfs.add(generator.generate());
+
+  const cpu = new CPU(fcfs, 5, 300);
+  console.log("=== Executando First Come, First Served ===");
+  await cpu.run();
+}
+
+async function runSJF() {
+  const generator = new ProcessGenerator();
+  const sjf = new SJFScheduler();
+
+  sjf.add(generator.generate());
+  sjf.add(generator.generate());
+  sjf.add(generator.generate());
+
+  const cpu = new CPU(sjf, 5, 300);
+  console.log("=== Executando Shortest Job First ===");
+  await cpu.run();
+}
+
+await runRR();
+await runFCFS();
+await runSJF();
