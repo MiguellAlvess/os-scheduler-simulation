e1b6a2c7916d87c625777d49a848fbcbed6273ea
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-02 18:36:47 -0300

    test: ensure clockCycle decrements the number of instructions

---
+++ src/entities/process.test.ts
@@ -0,0 +1,17 @@
+import { Process } from "./process.js";
+
+describe("Process", () => {
+  test("Deve garantir que o clockCycle decremente a quantidade de instruções corretamente", () => {
+    const processo = new Process(1, 3);
+
+    expect(processo.clockCycle()).toBe(
+      "Processo 1 executando. Restam 2 instruções.",
+    );
+    expect(processo.getInstructionCount()).toBe(2);
+
+    processo.clockCycle();
+    processo.clockCycle();
+    expect(processo.getInstructionCount()).toBe(0);
+    expect(processo.clockCycle()).toBe("Processo 1 já finalizado.");
+  });
+});
